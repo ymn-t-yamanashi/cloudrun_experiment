f822025e600b035fdaeae2590a3dedcea429dcce
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-07 20:47:05 +0900

    修正

--- config/runtime.exs
+++ config/runtime.exs
@@ -26,14 +26,13 @@ if config_env() == :prod do
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base = "secret_key_base"
     # System.get_env("SECRET_KEY_BASE") ||
     #   raise """
     #   environment variable SECRET_KEY_BASE is missing.
     #   You can generate one by calling: mix phx.gen.secret
     #   """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  #host = System.get_env("PHX_HOST") || "example.com"
   #port = String.to_integer(System.get_env("PORT") || "8080")
 
   config :cloudrun_experiment, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -48,7 +47,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: 8080
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: "tVpEOPsxyRCvW4uQTtOD3xqz1xkn+0UsRR3gIfJQ/nlrqfaiieu69oCi9+9MNsfJ",
+    server: true
 
   # ## SSL Support
   #
